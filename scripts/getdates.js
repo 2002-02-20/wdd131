ae8bc9cb7292ab76e5b50c6ffeb0bf6e32e12dad
Derek <adhoc607@gmail.com>
2025-10-05 20:15:52 -0500

    form completed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,50 @@
-document.getElementById('lastModified').textContent = new Date().toLocaleString('en-GB', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: false
-        }).replace(',', '');
+document.getElementById("lastModified").textContent = new Date()
+  .toLocaleString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  })
+  .replace(",", "");
+
+//ARRAY FORM
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5,
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7,
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5,
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9,
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0,
+  },
+];
+
+const productSelect = document.getElementById("product");
+
+products.forEach((product) => {
+  const option = document.createElement("option");
+  option.value = product.id;
+  option.textContent = product.name;
+  productSelect.appendChild(option);
+});
+
--- scripts/math.js
+++ scripts/math.js
@@ -1,11 +1,41 @@
 
 let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
 let resultFilter = names.filter(name => name.startsWith('B'));
-console.log(resultFilter);
+//console.log(resultFilter);
 
 let resultMap = names.map((name) => name.length);
-console.log(resultMap);
+//console.log(resultMap);
 
 const initialValue = 0;
 let resultReduce = names.reduce((a,b) => a + b.length, initialValue) /names.length;
-console.log(resultReduce);
+//console.log(resultReduce);
+
+function calculate(a, b, callback) {
+  callback(a + b);
+}
+
+function displayResult(result) {
+  //console.log('The result is: ' + result);
+}
+
+//calculate(2, 3, displayResult)
+const nums = [1,2,3,4,5];
+nums.forEach(function(num) {
+ //console.log(num);
+});
+
+
+const a = ['Jimmy Shah','Ram Sharma','Vikram Gupta','Shyam Sharma'];
+const result = a.filter(name => name.includes('Sharma'))
+
+function arrayFunction(a, callback) {
+    const result = a.filter(name => name.includes('Sharma'));
+    callback(result);
+}
+
+function displayResult(result) {
+  console.log('The result is: ' + result);
+}
+
+
+arrayFunction(a, displayResult);
